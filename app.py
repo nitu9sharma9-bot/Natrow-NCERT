adb34a0eca9fbfc6ebbe25b02cddef0fb5d18782
Nitu <nitu9sharma9@gmail.com>
2026-05-27 14:59:13 +0530

    fixed port error

--- app.py
+++ app.py
@@ -1110,6 +1110,4 @@ def delete_connection():
 
 # ── RUN ──
 if __name__ == "__main__":
-    app.run(debug=True)
-    port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
